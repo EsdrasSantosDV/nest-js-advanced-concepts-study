e1589ef89cce94299dcc1c8b2ff09dff81bce1d3
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2025-02-16 14:55:34 -0300

    feat:metadata hjost interval

--- src/scheduler/interval.scheduler/interval.scheduler.ts
+++ src/scheduler/interval.scheduler/interval.scheduler.ts
@@ -1,11 +1,21 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-import { DiscoveryService, Reflector } from '@nestjs/core';
+import {
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
+import { DiscoveryService, MetadataScanner, Reflector } from '@nestjs/core';
+import { INTERVAL_KEY } from '../decorators/interval.decorator';
 
 @Injectable()
-export class IntervalScheduler implements OnApplicationBootstrap {
+export class IntervalScheduler
+  implements OnApplicationBootstrap, OnApplicationShutdown
+{
+  private readonly intervals: NodeJS.Timer[] = [];
+
   constructor(
     private readonly discoveryService: DiscoveryService,
     private readonly reflector: Reflector,
+    private readonly metadataScanner: MetadataScanner, //ESSE JA CONSEGUIMOS PEGAR OS METODOS E O METADATA DOS METODOS
   ) {}
   onApplicationBootstrap(): any {
     const providers = this.discoveryService.getProviders();
@@ -25,6 +35,22 @@ export class IntervalScheduler implements OnApplicationBootstrap {
       }
       //COM ISSO CONSEGUIMOS PEGAR TODOS OS PROVIDERS QUE TEM O DECORATOR INTERVAL_HOST
       console.log('IntervalHost', provider.token);
+
+      const methodKeys = this.metadataScanner.getAllMethodNames(prototype);
+      methodKeys.forEach((methodKey) => {
+        const interval = this.reflector.get(INTERVAL_KEY, instance[methodKey]);
+        if (interval === undefined) {
+          return;
+        }
+        const intervalRef = setInterval(() => instance[methodKey](), interval);
+        this.intervals.push(intervalRef);
+      });
     });
+  }
+
+  onApplicationShutdown(signal?: string) {
+    this.intervals.forEach((intervalRef) =>
+      clearInterval(intervalRef as unknown as number),
+    );
   }
 }

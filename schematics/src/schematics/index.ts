be5b73aba80d92777649f1cee0bf6609d8dc0f8a
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2025-02-16 14:13:59 -0300

    feat:schematics wip

--- schematics/src/schematics/index.ts
+++ schematics/src/schematics/index.ts
@@ -1,10 +1,14 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function schematics(_options: any): Rule {
+export function generate(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    //NUMA SCHEMAATICS REPRESENTA POR UMA ARVORE
+    //que contains the files and directories in the project
+    //AQYU DEFUBE AS REFGRAS DA SCGENATUCS
+    //CREATE,RENAME,DELETE,OVERWRITE
+    //ESSAS SAO AS POSIVEIS ACTIONS DA MINHA SCHEMATIC
     return tree;
   };
 }
